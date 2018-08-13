d6877314ee9b5d9e05e30c31a5049c776bad9aa6
Andreas Schildbach <andreas@schildbach.de>
2018-08-13 11:45:19 +0200

    NetworkProviderFactory: Update user agent.

--- oeffi/src/de/schildbach/oeffi/network/NetworkProviderFactory.java
+++ oeffi/src/de/schildbach/oeffi/network/NetworkProviderFactory.java
@@ -81,7 +81,7 @@ import okhttp3.HttpUrl;
 public final class NetworkProviderFactory {
     private static Map<NetworkId, NetworkProvider> providerCache = new HashMap<>();
 
-    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
 
     public static synchronized NetworkProvider provider(final NetworkId networkId) {
         final NetworkProvider cachedNetworkProvider = providerCache.get(networkId);
